ebe0bb9e8c23018b5adf0dba4514bdc1c58786eb
Andre Henriques <andrehenriquesb@gmail.com>
2023-06-29 21:42:01 -0300

    estudos

---
+++ bytebank_GeradorChavePix/bytebank_GeradorChavePix/GeradorPix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace bytebank_GeradorChavePix
+{
+	/// <summary>
+	/// Classe que gera chaves PIX usando o formato Guid.
+	/// </summary>
+	public static class GeradorPix
+	{
+		/// <summary>
+		/// Método que retorna uma chave aleatória Pix
+		/// </summary>
+		/// <returns>Retorna uma chave Pix no formato String.</returns>
+		public static string GetChavePix()
+		{
+			return Guid.NewGuid().ToString();
+		}
+
+        /// <summary>
+        /// Método que retorna uma lista aleatória de chaves Pix
+        /// </summary>
+		/// <param name="numeroChaves">Quantidade de chaves a serem geradas.</param>
+        /// <returns>Lista de strings de chaves Pix.</returns>
+        public static List<string> GetChavesPix(int numeroChaves)
+		{
+            var chaves = new List<string>();
+            if (numeroChaves <= 0)
+			{
+				return chaves;
+            }
+			else
+			{
+				
+				for (int i = 0; i < numeroChaves; i++)
+				{
+					chaves.Add(Guid.NewGuid().ToString());
+				}
+				return chaves;
+			}
+		}
+	}
+}
+
---
+++ curso_bibliotecas-Aula01/bytebank_Modelos/bytebank.Modelos/ADM/Utilitario/AutenticacaoUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace bytebank_Modelos.bytebank.Modelos.ADM.Utilitario
+{
+	public class AutenticacaoUtil
+	{
+		public bool ValidarSenha(string senhaVerdadeira, string senhaTentativa)
+		{
+			return senhaVerdadeira.Equals(senhaTentativa);
+        }
+	}
+}
+
